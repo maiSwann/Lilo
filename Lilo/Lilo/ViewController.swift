b324603eac3ddf1093da48097994db6393a435c2
maiSwann <62833315+maiSwann@users.noreply.github.com>
2020-12-29 17:04:40 +0100

    Revert "[ADD] HomeVC implemented with Map View and HomeVC controller created"

---
+++ Lilo/Lilo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lilo
+//
+//  Created by Maïlys Perez on 29/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
