13507fdd20f764816d6d1b74a75eab1cd62badec
mailys <mailys.perez@epitech.eu>
2020-12-29 18:04:35 +0100

    [ADD] request user services for location added and user location displayed on the map

--- Lilo/Lilo/Controller/HomeVC.swift
+++ Lilo/Lilo/Controller/HomeVC.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class HomeVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+    }
+}
+
+extension HomeVC: MKMapViewDelegate {
+}
+
+extension HomeVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if locationManager.authorizationStatus == .notDetermined {
+             locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
     }
 }
