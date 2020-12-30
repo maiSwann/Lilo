5bac101b976e76f2f86549cdf3129e645d6997f1
mailys <mailys.perez@epitech.eu>
2020-12-30 16:14:44 +0100

    [ADD] added dependencies for swiftyJSON, a stationService to handle and parse the StationsInfos from the VELIB API, an algorithm to search for the ten nearest stations from the user location

---
+++ Lilo/Lilo/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Lilo
+//
+//  Created by Maïlys Perez on 30/12/2020.
+//
+
+import Foundation
+
+// rename a type by a closure
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let URL_STATIONS_INFOS = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json"
